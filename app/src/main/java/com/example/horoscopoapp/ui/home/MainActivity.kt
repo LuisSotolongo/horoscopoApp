ae6461f9d43c21eabfe05b4c4023fd1f541defaa
Luis Rene Rodriguez Sotolongo <lurosot@gmail.com>
2023-09-04 23:07:28 +0100

    added colors in color res

--- app/src/main/java/com/example/horoscopoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.horoscopoapp
+package com.example.horoscopoapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.horoscopoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
